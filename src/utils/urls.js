9e7d9aa0acf3e22abd507442b61af5d7c22be588
nareksvazyan <65123969+nareksvazyan@users.noreply.github.com>
2026-02-12 22:51:30 +0400

    Update API and images URLs to new endpoints

--- src/utils/urls.js
+++ src/utils/urls.js
@@ -1,4 +1,4 @@
-export const apiUrl = "https://beglaryan-backend-1.onrender.com/api";
-export const imagesUrl = "https://beglaryan-backend-1.onrender.com";
+export const apiUrl = "https://ggrc-backend-hr0d.onrender.com/api";
+export const imagesUrl = "https://ggrc-backend-hr0d.onrender.com";
 // export const apiUrl = "https://beglaryan-bac.onrender.com/api";
 // export const imagesUrl = "https://beglaryan-bac.onrender.com";
